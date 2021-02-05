af1b2bec280066f0fefcea9c0d8f94ce7fc08e3b
Ptolemy <62045440+Ptolemae@users.noreply.github.com>
2021-02-06 00:24:01 +1100

    Style + Cleanup + Language Button + Localization
    Changed some styling of the welcome Message.
    Text is now localized (Needs Translations)
    Cleaned some codee up.

--- src/Modules/Welcome/Welcome.js
+++ src/Modules/Welcome/Welcome.js
@@ -1,49 +1,12 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import PropTypes from "prop-types";
+// import DialogContentText from "@material-ui/core/DialogContentText";
+// import DialogTitle from "@material-ui/core/DialogTitle";
 import { makeStyles } from "@material-ui/core/styles";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Box from "@material-ui/core/Box";
-import AppBar from "@material-ui/core/AppBar";
 import logo from "../../Images/QeAssets/QELogo.png";
-import { Typography } from "@material-ui/core";
+import { Button, Box, Dialog, DialogActions, DialogContent, Typography, Grid } from "@material-ui/core";
 import ls from "local-storage";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
-      {value === index && <Box p={3}>{children}</Box>}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    "aria-controls": `simple-tabpanel-${index}`,
-  };
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      backgroundColor: theme.palette.background.paper,
-    },
-  }));
-}
+import { useTranslation } from "react-i18next";
+import LanguageSelector from "../SetupAndMenus/Header/LanguageButton";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -53,13 +16,8 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function WelcomeDialog(props) {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
   const [open, setOpen] = React.useState(ls.get("welcomeMessage") === true ? false : true);
+  const { t } = useTranslation();
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -67,27 +25,45 @@ export default function WelcomeDialog(props) {
 
   const handleClose = () => {
     setOpen(false);
-    props.handleClickOpen()
+    props.handleClickOpen();
   };
 
   return (
     <div>
       <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open form dialog
+        Test Welcome Message
       </Button>
-      <Dialog open={open} aria-labelledby="form-dialog-title">
-        {/* <DialogTitle id="form-dialog-title">Subscribe</DialogTitle> */}
+      <Dialog open={open}>
         <DialogContent>
-            <Typography align="center" variant="h3">Welcome to</Typography> 
-          <img style={{height: "100%", width: "100%"}} src={logo} alt="QE Live" />
-          <Typography align="center" variant="h6">To continue please create a character!</Typography> 
-          {/* <DialogContentText>To subscribe to this website, please enter your email address here. We will send updates occasionally.</DialogContentText>
-          <TextField autoFocus margin="dense" id="name" label="Email Address" type="email" fullWidth /> */}
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Typography align="center" variant="h3">
+                {t("Welcome.WelcomeTo")}
+              </Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <img style={{ height: "100%", width: "100%" }} src={logo} alt="QE Live" />
+            </Grid>
+            <Grid item xs={12}>
+              <Typography align="center" variant="h6">
+                {t("Welcome.ContinueMessage")}
+              </Typography>
+            </Grid>
+          </Grid>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            Create Character
-          </Button>
+          <div
+            style={{
+              display: "inline-flex",
+              width: "100%",
+              justifyContent: "space-between",
+            }}
+          >
+            <LanguageSelector />
+            <Button onClick={handleClose} color="primary">
+              {t("Welcome.CreateCharacter")}
+            </Button>
+          </div>
         </DialogActions>
       </Dialog>
     </div>
